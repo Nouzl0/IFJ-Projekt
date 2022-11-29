3e10519830ae5a02a56f8a964767e3a15c68e346
Nikolas Nosál <nikolas.nosal.0@gmail.com>
2022-11-29 20:48:22 +0100

    code_generation.h - done (ifelse, whileblock)
    can create code from if-else and while cycles

---
+++ ifelse_example.php
@@ -0,0 +1,28 @@
+<?php
+    declare(strict_types=1);
+
+    if ($a == 0) {
+    
+    } else {
+
+    }
+
+    if ($a == 0) {
+        if ($b == 0) { 
+        
+        } else {
+
+        }
+    } else {
+        if ($d == 0) {
+            if ($c == 0) {
+            
+            } else {
+            
+            }
+        } else {
+        
+        }
+    } 
+
+?>
---
+++ while_example.php
@@ -0,0 +1,18 @@
+<?php
+    declare(strict_types=1);
+    
+    
+    while($a == 5) {
+        $a = $a + 1;
+    }
+
+    while ($a == 10) {
+        while ($b == 20) {
+            while ($c == 30) {
+                while ($d == 40) {
+                }
+            }
+        }
+    }
+
+?>
