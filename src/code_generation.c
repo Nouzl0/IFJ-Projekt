f42cf5f9a864726fcbf7ad77590bf25de68c4e61
Nikolas Nosál <nikolas.nosal.0@gmail.com>
2022-12-07 22:07:07 +0100

    code_generation.h - fixed Werrors

--- src/code_generation.c
+++ src/code_generation.c
@@ -958,7 +958,6 @@ void func_print(expr_node_t* AP_Tree, char *token_content) {
     
     // #0 - variables
     static int func_call = 0;
-    static int first_params_len = 0;
 
     // #1 - checking if the function is language-defined
     int func_num = is_defined_func(AP_Tree);
@@ -1126,7 +1125,7 @@ void func_write(expr_node_t* AP_Tree)
             // go to bottom
             func_print(AP_Tree->params[i], NULL); 
             // define variables
-            printf("WRITE GF@%%freturn\n", i);
+            printf("WRITE GF@%%freturn\n");
         }
 
         // if arithmetic expression
